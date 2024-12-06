c86af7c93112d6dcacb7ffce2e235cbf93866b8f
AMIRI <mohamed69200>
2024-12-06 13:58:37 +0100

    Add global styles for app consistency

---
+++ lib/styles/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle titleStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle subtitleStyle = TextStyle(
+    fontSize: 16,
+    color: Colors.grey,
+  );
+}
